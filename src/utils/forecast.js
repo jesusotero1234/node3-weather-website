47466f7da32738954a95e2c667cb7364fe6429de
Jesus Otero <jesusotero@Jesuss-MacBook-Pro.local>
2019-11-04 16:28:14 +0000

    new feature added to the forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,4 +1,5 @@
 const request = require('request')
+const moment = require('moment')
 
 const forecast = (latitude, longitude, callback) =>{
     const url = `https://api.darksky.net/forecast/0db8ddf36f7182212382104dbcb7aefd/${latitude},${longitude}?units=si`
@@ -8,7 +9,9 @@ const forecast = (latitude, longitude, callback) =>{
         if (error) { callback('Unable to connect the weather service') }
         else if (body.error) { callback('Unable to find your location') }
         else {
-            callback(undefined,`it is currently ${data.temperature} degrees out. There is a ${data.precipProbability}% chance of rain`)
+            debugger
+            const time = moment().format((data.time,"dddd, MMMM Do YYYY, h:mm:ss a"))
+            callback(undefined,`it is currently ${data.temperature} degrees out. There is a ${data.precipProbability}% chance of rain.` && `\n` && `${time}` )
         }
     })
 }
